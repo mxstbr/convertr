a9e5d38d425708f68462068a99f1512743af6396
Max Stoiber <contact@mstoiber.com>
2015-03-16 15:02:57 +0000

    Added british spelling

--- Source/content_script.js
+++ Source/content_script.js
@@ -36,17 +36,17 @@ var imperialUnits = {
 
 var metricUnits = {
 	// Length
-	"millimeter": ["mm", "millimeter", "millimeters", "milimeter", "milimeters"],
-	"centimeter": ["cm", "centimeter", "centimeters"],
-	"meter"		: ["m", "meter", "meters"],
-	"kilometer"	: ["km", "kilometer", "kilometers"],
+	"millimeter": ["mm", "millimeter", "millimeters", "milimeter", "milimeters", "millimetre", "millimetres", "milimetre", "milimetres"],
+	"centimeter": ["cm", "centimeter", "centimeters", "centimetre", "centimetres"],
+	"meter"		: ["m", "meter", "meters", "metre", "metres"],
+	"kilometer"	: ["km", "kilometer", "kilometers", "kilometre", "kilometres"],
 	// Mass
 	"milligram"	: ["mg", "milligram", "milligrams", "miligram", "miligrams"],
 	"gram" 		: ["g", "gram", "grams"],
 	"kilogram"	: ["kg", "kilogram", "kilograms"],
 	// Volume
-	"milliliter": ["ml", "milliliter", "milliliters", "mililiter", "mililiters"],
-	"liter"		: ["l", "liter", "liters"],
+	"milliliter": ["ml", "milliliter", "milliliters", "mililiter", "mililiters", "millilitre", "millilitres", "mililitre", "mililitres"],
+	"liter"		: ["l", "liter", "liters", "litre", "litres"],
 	// Temperature
 	"celsius"	: ["C", "° C", "°C", "degrees C", "degrees Celsius", "Celsius"],
 	// Speed
