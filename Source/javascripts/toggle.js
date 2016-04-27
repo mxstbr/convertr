5b0f9de7fbbfddff9752f0274ac47db75d025b4e
Moritz Kobrna <github@kobrna.net>
2016-04-27 16:39:13 +0200

    fix: default settings where missing

--- Source/javascripts/toggle.js
+++ Source/javascripts/toggle.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 
 import OptionsPopup from './containers/OptionsPopup';
 
+import defaultSettings from './constants/defaultSettings';
+
 chrome.storage.sync.get('settings', (data) => {
-	ReactDOM.render(<OptionsPopup settings={data.settings} />, document.getElementById('app'));
+	const settings = data.settings || defaultSettings;
+	ReactDOM.render(<OptionsPopup settings={settings} />, document.getElementById('app'));
 });
